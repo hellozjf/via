c0251d2644e2f27e6f77bcdfe2aa6b3be77ab29a
hellozjf <908686171@qq.com>
2018-07-05 11:32:43 +0800

    init

---
+++ src/main/java/com/hellozjf/via/ViaApplication.java
@@ -0,0 +1,12 @@
+package com.hellozjf.via;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaApplication.class, args);
+	}
+}
